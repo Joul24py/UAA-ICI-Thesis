1237cdc3fc4f77e3add2154e4f8c1ab94765d591
Joul Espinoza Sánchez <alex.esa18@gmail.com>
2022-12-26 20:21:21 -0600

    Primer intento de recibir señales en vivo y eliminar el CSV cada 10 segundos

--- 02-code/02-code/00-mouse-position.py
+++ 02-code/02-code/00-mouse-position.py
@@ -2,7 +2,10 @@ import pyautogui
 import time
 
 # Sleeping the script so we have time to set the mouse on its position
-time.sleep(10)
+time.sleep(3)
 
 # Getting the mouse position
 print(pyautogui.position())
+
+# 1 (loop): Point(x=408, y=61)
+# 1 (stop): Point(x=319, y=60)
